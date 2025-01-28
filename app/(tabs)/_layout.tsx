df8f0046c5dc2007de01c06fc87c8d642eeea152
kpicoder <vladshcheglov41@gmail.com>
2025-01-28 02:44:59 -0500

    renamed icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -34,7 +34,7 @@ export default function TabLayout() {
                 <Tabs.Screen
                     name="explore"
                     options={{
-                        title: 'Explore',
+                        title: 'Manage',
                         tabBarIcon: ({color}) => <IconSymbol size={28} name="paperplane.fill" color={color}/>,
                     }}
                 />
