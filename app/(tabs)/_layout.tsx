ea95d537d589e6773e502aa8f15a225f862c0cf9
kpicoder <vladshcheglov41@gmail.com>
2025-01-27 22:45:57 -0500

    moved meals context a level lower

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,35 +2,38 @@ import {Tabs} from "expo-router";
 import {Platform} from "react-native";
 import React from "react";
 import {IconSymbol} from "@/components/ui-kit/IconSymbol";
+import MealsDataProvider from "@/modules/Meal/contexts/MealsDataContext";
+import {mealsHttp} from "@/modules/Meal/repository/MealsHttp";
 
 export default function TabLayout() {
 
     return (
-        <Tabs
-            screenOptions={{
-                headerShown: false,
-                tabBarStyle: Platform.select({
-                    ios: {
-                        // Use a transparent background on iOS to show the blur effect
-                        position: 'absolute',
-                    },
-                    default: {},
-                }),
-            }}>
-            <Tabs.Screen
-                name="index"
-                options={{
-                    title: 'Home',
-                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-                }}
-            />
-            <Tabs.Screen
-                name="explore"
-                options={{
-                    title: 'Explore',
-                    tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-                }}
-            />
-        </Tabs>
+        <MealsDataProvider mealRepository={mealsHttp}>
+            <Tabs
+                screenOptions={{
+                    headerShown: false,
+                    tabBarStyle: Platform.select({
+                        ios: {
+                            position: 'absolute',
+                        },
+                        default: {},
+                    }),
+                }}>
+                <Tabs.Screen
+                    name="index"
+                    options={{
+                        title: 'Home',
+                        tabBarIcon: ({color}) => <IconSymbol size={28} name="house.fill" color={color}/>,
+                    }}
+                />
+                <Tabs.Screen
+                    name="explore"
+                    options={{
+                        title: 'Explore',
+                        tabBarIcon: ({color}) => <IconSymbol size={28} name="paperplane.fill" color={color}/>,
+                    }}
+                />
+            </Tabs>
+        </MealsDataProvider>
     );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,14 @@
 import {Stack} from "expo-router";
 import {StatusBar} from "expo-status-bar";
-import MealsDataProvider from "@/modules/Meal/contexts/MealsDataContext";
-import {mealsHttp} from "@/modules/Meal/repository/MealsHttp";
 
 export default function RootLayout() {
     return (
-        <MealsDataProvider mealRepository={mealsHttp}>
+        <>
             <Stack>
                 <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
             </Stack>
             <StatusBar style="auto"/>
-        </MealsDataProvider>
+        </>
     )
 
 }
